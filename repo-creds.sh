5f2a646de2b5dccf756c545518894b3a37e0071c
eshanized <m.eshanized@gmail.com>
2025-01-06 23:48:04 +0530

    🐛 fix: url for keyring and mlist

--- repo-creds.sh
+++ repo-creds.sh
@@ -1,4 +1,4 @@
-pacman-key --recv-key 93BC54415EA8E7AB --keyserver keyserver.ubuntu.com
-pacman-key --lsign-key 93BC54415EA8E7AB
-pacman -U '#'
-pacman -U '@'
+sudo pacman-key --recv-key 93BC54415EA8E7AB --keyserver keyserver.ubuntu.com
+sudo pacman-key --lsign-key 93BC54415EA8E7AB
+sudo pacman -U 'https://github.com/Snigdha-OS/snigdhaos-core/blob/master/x86_64/snigdhaos-keyring-r332.68a02606-1-any.pkg.tar.zst'
+sudo pacman -U 'https://github.com/Snigdha-OS/snigdhaos-core/blob/master/x86_64/snigdhaos-mirrorlist-r331.bdb761c5-1-any.pkg.tar.zst'
