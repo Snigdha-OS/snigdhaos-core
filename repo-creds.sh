22b4634e0daad4766657b2adb1d17f7f44ad5caa
CELESTIFYX <celestifyx@gmail.com>
2025-01-14 19:17:02 +0200

    Updated all packages

--- repo-creds.sh
+++ repo-creds.sh
@@ -1,4 +1,4 @@
-sudo pacman-key --recv-key 93BC54415EA8E7AB --keyserver keyserver.ubuntu.com
-sudo pacman-key --lsign-key 93BC54415EA8E7AB
-sudo pacman -U 'https://github.com/Snigdha-OS/snigdhaos-core/blob/master/x86_64/snigdhaos-keyring-r332.68a02606-1-any.pkg.tar.zst'
-sudo pacman -U 'https://github.com/Snigdha-OS/snigdhaos-core/blob/master/x86_64/snigdhaos-mirrorlist-r331.bdb761c5-1-any.pkg.tar.zst'
+sudo pacman-key --recv-key 1CC586C7A25E47C5 --keyserver keyserver.ubuntu.com
+sudo pacman-key --lsign-key 1CC586C7A25E47C5
+sudo pacman -U 'https://github.com/Snigdha-OS/snigdhaos-core/blob/master/x86_64/snigdhaos-keyring-1.0.0-1-any.pkg.tar.zst'
+sudo pacman -U 'https://github.com/Snigdha-OS/snigdhaos-core/blob/master/x86_64/snigdhaos-mirrorlist-1.0.0-1-any.pkg.tar.zst'
